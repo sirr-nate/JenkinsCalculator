b23c2e7c6b5886fe0b08f279dc08f2a861497a72
sirrinat <nathansirrine@gmail.com>
2022-02-18 14:50:16 -0700

    Implement fibonacci

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -38,8 +38,25 @@ class Calculator {
     .
     etc
      */
-    int fibonacciNumberFinder(int n){
-        return 0;
+    int fibonacciNumberFinder(int n) {
+        /* Declare an array to store Fibonacci numbers. */
+        int f[] = new int[n + 1];
+        int i;
+
+        /* 0th and 1st number of the series are 0 and 1*/
+        f[0] = 0;
+
+        if (n > 0) {
+            f[1] = 1;
+
+            for (i = 2; i <= n; i++) {
+                /* Add the previous 2 numbers in the series
+             and store it */
+                f[i] = f[i - 1] + f[i - 2];
+            }
+        }
+
+        return f[n];
     }
 
 
